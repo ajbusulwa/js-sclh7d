15df6cb0454817a796cbcce6eb19f663c9164c30
ajbusulwa <anthonybusulwa@gmail.com>
2023-07-08 03:41:10 +0800

    2 files modified

--- index.js
+++ index.js
@@ -10,24 +10,46 @@ function App() {
     setIngredients(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Make API request to ChatGPT with ingredients
-    // Update mealSuggestions state with API response
-    // Display meal suggestions on the UI
+
+    try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer YOUR_API_KEY',
+        },
+        body: JSON.stringify({
+          messages: [
+            { role: 'system', content: 'You: ' + ingredients },
+            { role: 'system', content: 'You: What can I cook with these ingredients?' },
+          ],
+        }),
+      });
+
+      const data = await response.json();
+
+      // Extract and update mealSuggestions state with the API response data
+      setMealSuggestions(data.choices.map((choice) => choice.message.content));
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
     <div className="container">
       <h1 className="title">Recipe Muse</h1>
       <form onSubmit={handleSubmit}>
-        <label>
+        <label className="ingredients-label">
           Ingredients:
-          <input
-            type="text"
+          <textarea
+            rows="4"
             value={ingredients}
             onChange={handleInputChange}
             placeholder="Enter your ingredients"
+            className="ingredients-textarea"
           />
         </label>
         <button type="submit" className="submit-button">Get Meal Suggestions</button>
